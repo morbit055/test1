53cc56f7839949b3634d4d1d05b2947b6150a159
Andrey Goloyda <andrey.goloyda@eosda.com>
2018-06-21 16:54:18 +0300

    321

---
+++ footer.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * The template for displaying the footer
+
+?>
+
+		</div><!-- .mt-container -->
+	</div><!-- #content -->
+
+	<?php
+		/**
+
+	     */
+	    do_action( 'k_footer' );
+	?>
+</div><!-- #page -->
+
+<?php
+	/**
+     * news_portal_after_page hook
+     *
+     * @since 1.0.0
+     */
+    do_action( 'k_after_page' );
+?>
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
