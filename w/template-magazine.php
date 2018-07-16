cdcf856ffa7d48eea1834a8bd9e8f30f846bc98b
Andrey Goloyda <andrey.goloyda@eosda.com>
2018-07-16 14:48:06 +0300

    e

---
+++ w/template-magazine.php
@@ -0,0 +1,71 @@
+<?php 
+/**
+*Template Name: Magazine post
+*Template Post Type: post
+*/
+
+
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+		<?php
+		while ( have_posts() ) : the_post();
+?>
+		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	
+
+	<div class="kk-article-thumb">
+		<?php the_post_thumbnail( 'full' ); ?>
+	</div><!-- .np-article-thumb -->
+
+	<header class="entry-header">
+		<?php 
+			the_title( '<h1 class="entry-title">', '</h1>' );
+			
+		?>
+		<div class="entry-meta">
+			<?php k_inner_posted_on(); ?>
+		</div><!-- .entry-meta -->
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		<?php
+			the_content( sprintf(
+				wp_kses(
+					/* translators: %s: Name of current post. Only visible to screen readers */
+					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'news-portal' ),
+					array(
+						'span' => array(
+							'class' => array(),
+						),
+					)
+				),
+				get_the_title()
+			) );
+
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'k' ),
+				'after'  => '</div>',
+			) );
+		?>
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+		<?php k_entry_footer(); ?>
+	</footer><!-- .entry-footer -->
+</article><!-- #post-<?php the_ID(); ?> -->	
+
+			
+<?php
+		endwhile; // End of the loop.
+		?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+k_get_sidebar();
+get_footer();
